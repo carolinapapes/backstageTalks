23f1bebc009176a0133e685caa4a6312cad40570
carolinapapes <papes.caro@gmail.com>
2023-02-27 22:00:49 -0300

    Footer basic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Footer() {
+export default function Footer({ className }) {
   return (
-    <div>Footer</div>
-  )
+    <div className={className}>
+      <div className="notes">
+        <p>
+          Backstage Talks is a magazine of casual, but in depth dialogues on
+          design and business. Our decisions shape and influence this complex
+          world—to have a chance to make the right ones, we need to talk.
+        </p>
+      </div>
+      <div className="contact">
+        <p>email@email</p>
+      </div>
+    </div>
+  );
 }
+
+Footer.propTypes = {
+  className: PropTypes.string,
+};
+
+Footer.defaultProps = {
+  className: '',
+};
